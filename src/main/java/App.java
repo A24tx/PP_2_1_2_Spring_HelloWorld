2985d6a43515624acb08a1466c0721fe64058c67
A24tx <56554344+A24tx@users.noreply.github.com>
2022-09-28 20:24:49 +0300

    решение

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
-@Component
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
