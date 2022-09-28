928df1f5c2629a8eb287574604f86e1faac29658
A24tx <56554344+A24tx@users.noreply.github.com>
2022-09-28 20:23:22 +0300

    Решение

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,22 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
+@Component
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat catTwo = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("hellowords -" + (bean == beanTwo));
+        System.out.println("cats -" + (cat == catTwo));
     }
 }
